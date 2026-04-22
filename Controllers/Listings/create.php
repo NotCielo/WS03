dd8c3fa0f8ae549bbbd1141d7b8cb7cd64a35a02
Lush Valera <lushvalera051527@gmail.com>
2026-04-22 11:45:24 +0800

    Fix router/includes/routes registration and error views

---
+++ Controllers/Listings/create.php
@@ -0,0 +1,3 @@
+<?php
+    loadView('Listings/create');
+?>
--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,13 @@
 <?php
-    define('BASE_PATH', dirname(__DIR__));
+    define('BASE_PATH', realpath(__DIR__ . '/..'));
     require BASE_PATH . '/helpers.php';
-    loadView('home');
+
+    require basePath('router.php');
+    $routes = new Router();
+    require basePath('routes.php');
+
+    $uri = $_SERVER['REQUEST_URI']; // uniform resource identifier
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    $routes->route($uri, $method);
 ?>
