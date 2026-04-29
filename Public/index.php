b1dd2b448daec133786e0131e86411e70de82755
NotCielo <lushvalera051527@gmail.com>
2026-04-29 12:12:21 +0800

    Succesful Merge

--- Public/index.php
+++ Public/index.php
@@ -1,13 +1,5 @@
 <?php
     define('BASE_PATH', realpath(__DIR__ . '/..'));
     require BASE_PATH . '/helpers.php';
-
-    require basePath('router.php');
-    $routes = new Router();
-    require basePath('routes.php');
-
-    $uri = $_SERVER['REQUEST_URI']; // uniform resource identifier
-    $method = $_SERVER['REQUEST_METHOD'];
-
-    $routes->route($uri, $method);
+    loadView('home');
 ?>
